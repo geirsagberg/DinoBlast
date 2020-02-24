82085531827b4e97cb370d1d3b7c09ed2517f554
Geir Sagberg <geir.sagberg@gmail.com>
2020-02-24 19:35:01 +0100

    Add Swept Circle collision detection

---
+++ src/BunnyLand.DesktopGL/Components/CollisionHelper.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using BunnyLand.DesktopGL.Extensions;
+using Microsoft.Xna.Framework;
+using MonoGame.Extended;
+
+namespace BunnyLand.DesktopGL.Components
+{
+    public static class CollisionHelper
+    {
+        public static Vector2 CalculatePenetrationVector(CircleF circle, CircleF otherCircle, Vector2 velocity, Vector2 otherVelocity, Vector2 oldDistance)
+        {
+            var sumRadius = circle.Radius + otherCircle.Radius;
+            // Find quadratic formula coefficients:
+            // u^2 coefficient
+            var a = velocity.Dot(velocity);
+            // u coefficient
+            var b = 2 * velocity.Dot(oldDistance);
+            // constant
+            var c = oldDistance.Dot(oldDistance) - sumRadius * sumRadius;
+
+            if (oldDistance.Dot(oldDistance) <= sumRadius * sumRadius) {
+                // Already overlapping
+                return circle.CalculatePenetrationVector(otherCircle);
+            }
+
+            if (SolveQuadraticFormula(a, b, c, out var u0, out var u1)) {
+                var maxPenetrationAt = (u0 + u1) / 2;
+                circle.Position += velocity * maxPenetrationAt;
+                otherCircle.Position += otherVelocity * maxPenetrationAt;
+                return circle.CalculatePenetrationVector(otherCircle);
+            }
+            return Vector2.Zero;
+        }
+
+        private static bool SolveQuadraticFormula(in float a, in float b, in float c, out float u0, out float u1)
+        {
+            var squareRootPart = b * b - 4 * a * c;
+            if (squareRootPart > 0) {
+                // Two real solutions
+                u0 = (-b + (float)Math.Sqrt(squareRootPart)) / (2 * a);
+                u1 = (-b - (float) Math.Sqrt(squareRootPart)) / (2 * a);
+                return true;
+            }
+
+            // ReSharper disable once CompareOfFloatsByEqualityOperator
+            if (squareRootPart == 0) {
+                u0 = u1 = (-b + (float) Math.Sqrt(squareRootPart)) / (2 * a);
+                return true;
+            }
+
+            u0 = u1 = 0;
+
+            return false;
+        }
+    }
+}
--- src/BunnyLand.DesktopGL/Extensions/GeometryExtensions.cs
+++ src/BunnyLand.DesktopGL/Extensions/GeometryExtensions.cs
@@ -1,9 +1,9 @@
 using System;
-using System.Diagnostics;
-using System.Numerics;
+using System.Collections.Generic;
 using System.Runtime.CompilerServices;
+using Microsoft.Xna.Framework;
 using MonoGame.Extended;
-using Vector2 = Microsoft.Xna.Framework.Vector2;
+using Vector3 = System.Numerics.Vector3;
 
 namespace BunnyLand.DesktopGL.Extensions
 {
@@ -49,7 +49,8 @@ namespace BunnyLand.DesktopGL.Extensions
         }
 
         [MethodImpl(MethodImplOptions.AggressiveInlining)]
-        public static CircleF Inflate(this CircleF circle, float addRadius) => new CircleF(circle.Center, circle.Radius + addRadius);
+        public static CircleF Inflate(this CircleF circle, float addRadius) =>
+            new CircleF(circle.Center, circle.Radius + addRadius);
 
         public static void Wrap(this Transform2 transform, RectangleF levelSize)
         {
@@ -69,9 +70,8 @@ namespace BunnyLand.DesktopGL.Extensions
         }
 
 
-
         /// <summary>
-        /// Calculate a's penetration into b
+        ///     Calculate a's penetration into b
         /// </summary>
         /// <param name="a">The penetrating shape.</param>
         /// <param name="b">The shape being penetrated.</param>
@@ -95,15 +95,12 @@ namespace BunnyLand.DesktopGL.Extensions
             if (intersectingRectangle.IsEmpty) return Vector2.Zero;
 
             Vector2 penetration;
-            if (intersectingRectangle.Width < intersectingRectangle.Height)
-            {
+            if (intersectingRectangle.Width < intersectingRectangle.Height) {
                 var d = rect1.Center.X < rect2.Center.X
                     ? intersectingRectangle.Width
                     : -intersectingRectangle.Width;
                 penetration = new Vector2(d, 0);
-            }
-            else
-            {
+            } else {
                 var d = rect1.Center.Y < rect2.Center.Y
                     ? intersectingRectangle.Height
                     : -intersectingRectangle.Height;
@@ -115,20 +112,16 @@ namespace BunnyLand.DesktopGL.Extensions
 
         private static Vector2 PenetrationVector(CircleF circ1, CircleF circ2)
         {
-            if (!circ1.Intersects(circ2))
-            {
+            if (!circ1.Intersects(circ2)) {
                 return Vector2.Zero;
             }
 
             var displacement = Point2.Displacement(circ1.Center, circ2.Center);
 
             Vector2 desiredDisplacement;
-            if (displacement != Vector2.Zero)
-            {
+            if (displacement != Vector2.Zero) {
                 desiredDisplacement = displacement.NormalizedCopy() * (circ1.Radius + circ2.Radius);
-            }
-            else
-            {
+            } else {
                 desiredDisplacement = -Vector2.UnitY * (circ1.Radius + circ2.Radius);
             }
 
@@ -142,13 +135,11 @@ namespace BunnyLand.DesktopGL.Extensions
             var collisionPoint = rect.ClosestPointTo(circ.Center);
             var cToCollPoint = collisionPoint - circ.Center;
 
-            if (rect.Contains(circ.Center) || cToCollPoint.Equals(Vector2.Zero))
-            {
+            if (rect.Contains(circ.Center) || cToCollPoint.Equals(Vector2.Zero)) {
                 var displacement = Point2.Displacement(circ.Center, rect.Center);
 
                 Vector2 desiredDisplacement;
-                if (displacement != Vector2.Zero)
-                {
+                if (displacement != Vector2.Zero) {
                     // Calculate penetration as only in X or Y direction.
                     // Whichever is lower.
                     var dispx = new Vector2(displacement.X, 0);
@@ -156,30 +147,23 @@ namespace BunnyLand.DesktopGL.Extensions
                     dispx.Normalize();
                     dispy.Normalize();
 
-                    dispx *= (circ.Radius + rect.Width / 2);
-                    dispy *= (circ.Radius + rect.Height / 2);
+                    dispx *= circ.Radius + rect.Width / 2;
+                    dispy *= circ.Radius + rect.Height / 2;
 
-                    if (dispx.LengthSquared() < dispy.LengthSquared())
-                    {
+                    if (dispx.LengthSquared() < dispy.LengthSquared()) {
                         desiredDisplacement = dispx;
                         displacement.Y = 0;
-                    }
-                    else
-                    {
+                    } else {
                         desiredDisplacement = dispy;
                         displacement.X = 0;
                     }
-                }
-                else
-                {
+                } else {
                     desiredDisplacement = -Vector2.UnitY * (circ.Radius + rect.Height / 2);
                 }
 
                 var penetration = displacement - desiredDisplacement;
                 return penetration;
-            }
-            else
-            {
+            } else {
                 var penetration = circ.Radius * cToCollPoint.NormalizedCopy() - cToCollPoint;
                 return penetration;
             }
@@ -188,6 +172,145 @@ namespace BunnyLand.DesktopGL.Extensions
         private static Vector2 PenetrationVector(RectangleF rect, CircleF circ)
         {
             return -PenetrationVector(circ, rect);
+        }
+
+        public static Vector2 GetSupportVector(this IShapeF shape, Vector2 direction) =>
+            shape switch {
+                RectangleF rect => rect.ClosestPointTo(rect.Center + direction),
+                CircleF circle => circle.ClosestPointTo(circle.Center + direction),
+                _ => throw new NotImplementedException()
+            };
+
+        public static Point2 GetCenter(this IShapeF shape) => shape switch {
+            RectangleF rect => rect.Center,
+            CircleF circle => circle.Center,
+            _ => throw new NotImplementedException()
+        };
+
+        public static OverlapTestResult TestOverlap(this IShapeF first, IShapeF second)
+        {
+            var direction = Vector2.Zero;
+            var vertices = new List<Vector2>();
+            var result = EvolveResult.StillEvolving;
+            var iterations = 0;
+            while (iterations < 20 && result == EvolveResult.StillEvolving) {
+                result = EvolveSimplex(vertices, first, second, ref direction);
+                iterations++;
+            }
+
+            return new OverlapTestResult(result == EvolveResult.FoundIntersection, vertices, first, second);
+        }
+
+        private static Vector2 TripleProduct(Vector2 a, Vector2 b, Vector2 c)
+        {
+            var a3 = new Vector3(a.X, a.Y, 0);
+            var b3 = new Vector3(b.X, b.Y, 0);
+            var c3 = new Vector3(c.X, c.Y, 0);
+
+            var first = Vector3.Cross(a3, b3);
+            var second = Vector3.Cross(first, c3);
+
+            return new Vector2(second.X, second.Y);
+        }
+
+        private static EvolveResult EvolveSimplex(List<Vector2> vertices, IShapeF shapeA, IShapeF shapeB,
+            ref Vector2 direction)
+        {
+            switch (vertices.Count) {
+                case 0:
+                    direction = shapeB.GetCenter() - shapeA.GetCenter();
+                    break;
+
+                // flip the direction
+                case 1:
+                    direction *= 1;
+                    break;
+                case 2: {
+                    // line ab is the line formed by the first two vertices
+                    var ab = vertices[1] - vertices[0];
+                    // line a0 is the line from the first vertex to the origin
+                    var a0 = vertices[0] * -1;
+
+                    // use the triple-cross-product to calculate a direction perpendicular
+                    // to line ab in the direction of the origin
+                    direction = TripleProduct(ab, a0, ab);
+                    break;
+                }
+                case 3: {
+                    // calculate if the simplex contains the origin
+                    var c0 = vertices[2] * -1;
+                    var bc = vertices[1] - vertices[2];
+                    var ca = vertices[0] - vertices[2];
+
+                    var bcNormal = TripleProduct(ca, bc, bc);
+                    var caNormal = TripleProduct(bc, ca, ca);
+
+                    if (Vector2.Dot(bcNormal, c0) > 0) {
+                        // the origin is outside line bc
+                        // get rid of a and add a new support in the direction of bcNorm
+                        vertices.Remove(vertices[0]);
+                        direction = bcNormal;
+                    } else if (Vector2.Dot(caNormal, c0) > 0) {
+                        // the origin is outside line ca
+                        // get rid of b and add a new support in the direction of caNorm
+                        vertices.Remove(vertices[1]);
+                        direction = caNormal;
+                    } else {
+                        // the origin is inside both ab and ac,
+                        // so it must be inside the triangle!
+                        return EvolveResult.FoundIntersection;
+                    }
+
+                    break;
+                }
+                default:
+                    throw new ArgumentOutOfRangeException(nameof(vertices),
+                        $"Can't have simplex with {vertices.Count} vertixes!");
+            }
+
+            return AddSupport(vertices, shapeA, shapeB, direction)
+                ? EvolveResult.StillEvolving
+                : EvolveResult.NoIntersection;
+        }
+
+        private static bool AddSupport(List<Vector2> vertices, IShapeF shapeA, IShapeF shapeB, Vector2 direction)
+        {
+            var newVertex = CalculateSupport(shapeA, shapeB, direction);
+            vertices.Add(newVertex);
+            return direction.Dot(newVertex) >= 0;
+        }
+
+        private static Vector2 CalculateSupport(IShapeF shapeA, IShapeF shapeB, Vector2 direction)
+        {
+            var oppositeDirection = direction * -1;
+            var newVertex = shapeA.GetSupportVector(direction);
+            newVertex -= shapeB.GetSupportVector(oppositeDirection);
+            return newVertex;
+        }
+
+
+        private enum EvolveResult
+        {
+            NoIntersection,
+            FoundIntersection,
+            StillEvolving
+        }
+    }
+
+
+    public class OverlapTestResult
+    {
+        public bool Colliding { get; set; }
+        public List<Vector2> Simplex { get; set; }
+        public IShapeF ShapeA { get; set; }
+        public IShapeF ShapeB { get; set; }
+
+        public OverlapTestResult(bool colliding, List<Vector2> simplex, IShapeF shapeA, IShapeF shapeB)
+        {
+            Colliding = colliding;
+            Simplex = simplex;
+            ShapeA = shapeA;
+            ShapeB = shapeB;
         }
     }
 }
