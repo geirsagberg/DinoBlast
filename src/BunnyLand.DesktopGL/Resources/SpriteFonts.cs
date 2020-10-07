8b9ff3792d15afa6ed1a19dcd956247de32ea17d
Geir Sagberg <geir.sagberg@gmail.com>
2020-10-07 20:18:32 +0200

    Update dependencies

---
+++ src/BunnyLand.DesktopGL/Resources/SpriteFonts.cs
@@ -0,0 +1,12 @@
+using Microsoft.Xna.Framework.Content;
+using Microsoft.Xna.Framework.Graphics;
+
+namespace BunnyLand.DesktopGL.Resources
+{
+    public partial class SpriteFonts : ResourcesBase<SpriteFont>
+    {
+        public SpriteFonts(ContentManager contentManager) : base(contentManager)
+        {
+        }
+    }
+}
