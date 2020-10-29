6f11d17833cc2c6d7777ae127eb7692d2729118e
Peder Voldes Langdal <peder.langdal@bekk.no>
2020-10-29 14:34:53 +0100

    Added skeleton for hole puncher

---
+++ src/DinoBlast.Puncher/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace DinoBlast.Puncher
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
