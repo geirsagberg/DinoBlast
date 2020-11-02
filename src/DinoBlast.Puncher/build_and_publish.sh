7daeb118fcf96eb3434ab7cfa7d44ebeadf2476d
Peder Voldes Langdal <peder.langdal@bekk.no>
2020-11-02 22:54:19 +0100

    Small improvements to scripts

--- src/DinoBlast.Puncher/build_and_publish.sh
+++ src/DinoBlast.Puncher/build_and_publish.sh
@@ -1,5 +1,7 @@
 #! /usr/bin/env bash
 
+# Read here to authenticate: https://cloud.google.com/sdk/gcloud/reference/auth/configure-docker
+
 docker build -t eu.gcr.io/dinoblast/puncher .
 
 docker push eu.gcr.io/dinoblast/puncher:latest
